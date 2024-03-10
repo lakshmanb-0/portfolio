218bd32393edb179d55df1a3386fe383dc7c43f1
Lakshman <lakshmanram2259@gmail.com>
2024-03-10 13:05:38 +0530

    vercel

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { NextUIProvider } from "@nextui-org/react";
 import App from './App';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <NextUIProvider>
       <App />
+      <Analytics />
     </NextUIProvider>
   </React.StrictMode>
 );
